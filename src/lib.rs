e02373f10fbb8e4ad9746c874b8583a6f8e55a2b
br3w0r <daniildirun123@gmail.com>
2023-01-06 23:38:13 +0300

    implement connect_device

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
 mod bindings_gen;
 
+use async_std::{
+    io::{self, WriteExt},
+    net, task,
+};
+use lazy_static::lazy_static;
+use regex::Regex;
+
 use bindings_gen::{self as bg, DeviceConnectInfo};
 
-use libc::{c_char, c_void};
-use std::{borrow::BorrowMut, ffi::CString};
+use libc::c_void;
+use std::ffi::{CStr, CString};
+
+const CONN_PARAM_IP: &str = "IP";
+const CONN_PARAM_PORT: &str = "Port";
+const CONN_PARAM_MESSAGE: &str = "Message";
+
+lazy_static! {
+    static ref RE_IP: Regex = Regex::new(r"(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])").unwrap();
+}
 
 #[no_mangle]
 pub extern "C" fn mod_version() -> u8 {
@@ -77,12 +92,16 @@ pub unsafe extern "C" fn init(sel: *mut *mut c_void) {
     let m = Module {
         params: vec![
             ConnParamInfo {
-                name: CString::new("IP").unwrap(),
+                name: CString::new(CONN_PARAM_IP).unwrap(),
                 typ: bg::ConnParamType::ConnParamString,
             },
             ConnParamInfo {
-                name: CString::new("Password").unwrap(),
+                name: CString::new(CONN_PARAM_PORT).unwrap(),
                 typ: bg::ConnParamType::ConnParamInt,
+            },
+            ConnParamInfo {
+                name: CString::new(CONN_PARAM_MESSAGE).unwrap(),
+                typ: bg::ConnParamType::ConnParamString,
             },
         ],
     };
@@ -108,10 +127,153 @@ pub unsafe extern "C" fn destroy(sel: *mut c_void) {
     Handle(sel).destroy();
 }
 
+#[repr(u8)]
+pub enum ConnDeviceErr {
+    ConnDeviceErrorNone = 0,
+    ConnDeviceErrorConn = 1,
+    ConnDeviceErrorParams = 2,
+}
+
 #[no_mangle]
-pub unsafe extern "C" fn connect_device(
+pub extern "C" fn connect_device(handler: *mut c_void, confs: *mut bg::DeviceConnectConf) -> u8 {
+    if let Err(err) = connect_device_impl(handler, confs) {
+        err as _
+    } else {
+        ConnDeviceErr::ConnDeviceErrorNone as _
+    }
+}
+
+fn connect_device_impl(
     handler: *mut c_void,
-    connect_info: *mut bg::DeviceConnectConf,
-) -> u8 {
-    todo!()
+    confs: *mut bg::DeviceConnectConf,
+) -> Result<(), ConnDeviceErr> {
+    let conf = ConnConf::new(confs)?;
+
+    // TODO
+    println!("{:?}", conf);
+
+    if let Err(err) = task::block_on(send_message_async(conf.ip, conf.port, conf.message)) {
+        return Err(ConnDeviceErr::ConnDeviceErrorConn);
+    }
+
+    Ok(())
+}
+
+#[derive(Default, Debug)]
+struct ConnConf {
+    ip: String,
+    port: u16,
+    message: String,
+}
+
+impl ConnConf {
+    fn new(confs_raw: *mut bg::DeviceConnectConf) -> Result<Self, ConnDeviceErr> {
+        if confs_raw.is_null() {
+            return Err(ConnDeviceErr::ConnDeviceErrorParams);
+        }
+
+        let confs = unsafe {
+            std::slice::from_raw_parts(
+                (*confs_raw).connection_params,
+                (*confs_raw).connection_params_len as usize,
+            )
+        };
+
+        let mut res_conf = ConnConf::default();
+
+        for conf in confs {
+            if let Ok(name) = unsafe { CStr::from_ptr(conf.name) }.to_str() {
+                match name {
+                    CONN_PARAM_IP => {
+                        if let Some(ip) = c_parser::as_string(conf.value) {
+                            res_conf.ip = ip;
+                        } else {
+                            return Err(ConnDeviceErr::ConnDeviceErrorParams);
+                        }
+                    }
+                    CONN_PARAM_PORT => {
+                        if let Some(port) = c_parser::as_from_str::<u16>(conf.value) {
+                            res_conf.port = port;
+                        } else {
+                            return Err(ConnDeviceErr::ConnDeviceErrorParams);
+                        }
+                    }
+                    CONN_PARAM_MESSAGE => {
+                        if let Some(msg) = c_parser::as_string(conf.value) {
+                            res_conf.message = msg;
+                        } else {
+                            return Err(ConnDeviceErr::ConnDeviceErrorParams);
+                        }
+                    }
+                    _ => {
+                        return Err(ConnDeviceErr::ConnDeviceErrorParams);
+                    }
+                }
+            } else {
+                return Err(ConnDeviceErr::ConnDeviceErrorParams);
+            }
+        }
+
+        Ok(res_conf)
+    }
+
+    fn validate(&self) -> bool {
+        if !RE_IP.is_match(&self.ip) {
+            return false;
+        }
+
+        if self.port <= 0 {
+            return false;
+        }
+
+        if self.message.len() == 0 {
+            return false;
+        }
+
+        true
+    }
+}
+
+mod c_parser {
+    use std::{
+        ffi::{c_char, CStr},
+        str::FromStr,
+    };
+
+    pub fn str_from_c_char(raw: *mut c_char) -> String {
+        let cstr = unsafe { CStr::from_ptr(raw) };
+
+        String::from_utf8_lossy(cstr.to_bytes()).to_string()
+    }
+
+    pub fn as_string(raw: *mut c_char) -> Option<String> {
+        Some(str_from_c_char(raw))
+    }
+
+    pub fn as_from_str<F: FromStr>(raw: *mut c_char) -> Option<F> {
+        let s = str_from_c_char(raw);
+
+        let res = s.parse::<F>();
+        if let Ok(val) = res {
+            Some(val)
+        } else {
+            None
+        }
+    }
+}
+
+async fn send_message_async(host: String, port: u16, msg: String) -> io::Result<()> {
+    let addr = host + ":" + port.to_string().as_str();
+    let mut stream = net::TcpStream::connect(addr).await?;
+    let req = format!(
+        "GET /?msg={} HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n",
+        msg
+    );
+    stream.write_all(req.as_bytes()).await?;
+
+    // let mut stdout = io::stdout();
+    // let mut buf = vec![0; 1024];
+    // stream.read(&mut buf).await?;
+    // println!("{}", String::from_utf8(buf).unwrap());
+    Ok(())
 }
---
+++ test_server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("\"%s\": %s\n", r.URL.Path, strings.Join(r.URL.Query()["msg"], ", "))
+
+		fmt.Fprintf(w, "ok\n")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
